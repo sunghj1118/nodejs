15af229af7efd7afdc8d52093390b52f85b9e8f3
Hyunjoon Sung <sunghj1118@gmail.com>
2023-01-19 00:44:02 +0900

    Practiced loops and arrays in nodejs

---
+++ syntax/array.js
@@ -0,0 +1,12 @@
+// CRUD
+// Create array
+var arr = ['A', 'B', 'C', 'D'];
+// Read
+console.log(arr[1]);
+console.log(arr.length);
+//Update
+arr[2] = 3;
+console.log(arr);
+arr.push('E');
+console.log(arr);
+//Delete
