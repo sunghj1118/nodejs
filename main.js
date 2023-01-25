f9fe73f9ec6442f325c09bf88af03836f73142c4
Hyunjoon Sung <sunghj1118@gmail.com>
2023-01-25 18:32:33 +0900

    First attempt at taking user input for file creation

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
     </body>
     </html>
@@ -45,8 +46,7 @@ var app = http.createServer(function(request,response){
                 var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template);
-            })
-            
+            });
         } else{
             fs.readdir('./data', function(error, filelist){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -58,7 +58,39 @@ var app = http.createServer(function(request,response){
                 });
             });
         }
-    } else {
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+                <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+            <input type="submit">
+            </p>
+            </form>
+            `);
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if(pathname === '/create_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                response.writeHead(200);
+                response.end('success');
+            })
+        });
+    }else {
         response.writeHead(404);
         response.end('Not Found');
     }
