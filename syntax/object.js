b696fb462004a48eb8c51275b233076f134ccd2f
성현준 <sunghj1118@gmail.com>
2023-04-01 17:27:06 +0900

    Studied classes in nodejs

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['hyunjoon', 'k8805', 'nodejs'];
+console.log(members[1])
+
+var roles = {
+    'programmer':'hyunjoon',
+    'designer':'k8805',
+    'manager':'nodejs'
+}
+
+console.log(roles.designer)
